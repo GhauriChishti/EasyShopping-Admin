d25ebe231a5dbe5d61d4b85660d5721489c6ea4f
GhauriChishti <farooqghaurie@gmail.com>
2026-03-11 17:04:56 +0500

    Fix Firebase initialization and stabilize admin dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_options.dart';
 import 'screens/dashboard_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  try {
+    Firebase.app();
+  } catch (_) {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
